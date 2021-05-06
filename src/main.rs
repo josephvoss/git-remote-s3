9141a40f762f25c0611f36d58e1aa3e545f2abd6
Joseph Voss <jvoss@josephvoss.com>
2021-05-06 13:55:22 -0400

    Just use struct opts

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,27 @@
-//extern crate clap;
-//use clap::{AppSettings, Clap, Arg, App, SubCommand, crate_authors, crate_version};
-use clap::{AppSettings, Clap, crate_authors, crate_version};
+use structopt::StructOpt;
 use anyhow::{Context, Result};
 
-#[derive(Clap)]
-// Remote helper to fetch and push git objects to a S3 bucket
+#[derive(StructOpt)]
+/// Remote helper to fetch and push git objects to a S3 bucket
 struct Opts {
-    #[clap(short, long)]
-    #[clap(default_value = "~/.git-remote-s3.config", env = "GIT_S3_CONFIG")]
-    // Sets a custom config file
+    #[structopt(short, long)]
+    #[structopt(default_value = "~/.git-remote-s3.config", env = "GIT_S3_CONFIG")]
+    /// Sets a custom config file
     config: String,
-    // Name of remote repository
+    /// Name of remote repository
     remoteName: String,
-    // Name of remote S3 bucket
+    /// Name of remote S3 bucket
     remoteBucket: String,
-    #[clap(short, long)]
+    #[structopt(short, long)]
+    /// Enable verbose logging
     verbose: bool,
-    #[clap(short, long)]
+    #[structopt(short, long)]
+    /// Enable debug logging
     debug: bool,
 }
 
 fn main() -> Result<()> {
-    let opts: Opts = Opts::parse();
-
-    // Load vars
-//jkk    let config = matches.value_of("config").unwrap_or("~/.git-remote-s3.config");
-//jkk    let remote = matches.value_of("remote-name")
-//jkk        .with_context(|| format!("No remote-name argument specifed!"))?;
-//jkk    let bucket = matches.value_of("remote-bucket")
-//jkk        .with_context(|| format!("No remote-bucket argument specifed!"))?;
-    //println!("Value for config: {}", config);
+    let opts: Opts = Opts::from_args();
 
     // Set logging level
 
