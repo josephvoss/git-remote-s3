1d397e0ca861899e442ce6299a77ae306a14bb91
Joseph Voss <jvoss@josephvoss.com>
2021-05-06 15:03:58 -0400

    Add logging

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,22 @@
 mod cli;
+mod git_s3;
 
 use structopt::StructOpt;
 use anyhow::Result;
+use log::{trace, debug, info, warn, error};
 
 fn main() -> Result<()> {
     let opts = cli::Opts::from_args();
 
     // Set logging level - is this even supported?
+    stderrlog::new()
+        .module(module_path!())
+        .verbosity(opts.verbose)
+        .init()
+        .unwrap();
 
     // Build git_s3 object
+    let remote = git_s3::Remote::new(opts);
 
     // Lock stdin
 
